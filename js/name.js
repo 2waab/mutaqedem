fe90dcf3a6a4de037284829f77da14ce49d4bd9b
Awaab Abdelbagi <92119191+2waab@users.noreply.github.com>
2023-03-01 19:09:11 +0300

    Update name.js

--- js/name.js
+++ js/name.js
@@ -1,12 +1,23 @@
 let User = {
     name: "",
     map: "",
+    gender: "",
 };
 
 let userName = document.querySelector("#user-name");
 let mapName = document.querySelector("#nam-map");
 let btn = document.querySelector("#btn");
 let popup = document.querySelector("#btn-popup");
+let gender = document.querySelectorAll(".gender .box");
+
+gender.forEach(btn => {
+    btn.addEventListener("click", () => {
+        gender.forEach(e => {
+            e.classList.remove("active");
+        });
+        btn.classList.add("active");
+    });
+});
 
 btn.onclick = () => {
     if (userName.value === "" || mapName.value === "") {
@@ -14,6 +25,13 @@ btn.onclick = () => {
     } else {
         User.name = userName.value;
         User.map = mapName.value;
+        gender.forEach(btn => {
+            if (btn.classList.contains("active") && btn.classList.contains("male")) {
+                User.gender = "male";
+            } else if (btn.classList.contains("active") && btn.classList.contains("female")) {
+                User.gender = "female";
+            };
+        });
         window.localStorage.setItem("userInfo", JSON.stringify(User));
         btn.href = "mapaway.html";
     };
