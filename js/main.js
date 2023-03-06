5d3563d7598ddd2dda0714b879dbdc336d56550d
Awaab Abdelbagi <92119191+2waab@users.noreply.github.com>
2023-03-07 01:58:03 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let links = document.querySelectorAll(".navbar-nav li a");
-links.forEach(link => {
-    link.addEventListener("click", () => {
-        links.forEach(a => {
-            a.classList.remove("active");
-            link.classList.add("active");
-        });
-    });
-});
